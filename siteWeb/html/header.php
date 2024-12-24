b763b8766a60845062e322222e3816e35ef674c3
Singosirutonamikaze <yaodieudonnesingo60@gmail.com>
2024-12-24 00:37:45 +0000

    Elaboration des divers fichier header footer et body

---
+++ siteWeb/html/header.php
@@ -0,0 +1,27 @@
+<header class="home_page">
+    <nav>
+        <div class="logo">
+            <img src="../img/logo.png" alt="logo">
+        </div>
+
+        <div class="lien_de_navigation">
+            <ul>
+                <li><a href="#">Acceuil</a></li>
+                <li><a href="#">A propos</a></li>
+                <li><a href="#">Nos produits</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="search__user">
+            <div class="search_input">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="search" name="search" id="search" placeholder="Rechercher votre produit...">
+            </div>
+
+            <div class="user_acount">
+                <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+    </nav>
+</header>
