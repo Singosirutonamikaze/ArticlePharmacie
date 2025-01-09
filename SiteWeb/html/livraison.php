3d97e07ff14ff57cfb6f04de144aa5fd59746bf4
Singosirutonamikaze <yaodieudonnesingo60@gmail.com>
2025-01-09 15:01:24 +0000

    Mise à jour des fichiers php

--- SiteWeb/html/livraison.php
+++ SiteWeb/html/livraison.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livraison</title>
-    <link rel="stylesheet" href="../css/livraison.css">
+    <link rel="stylesheet" href="../css/css/livraison.css">
 </head>
 
 <body>
